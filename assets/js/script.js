9d8edc8df7b382d824c9c87ca22c6cbd7245b899
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-02-04 18:57:46 -0800

    added the map, sites, weather to the css and deleted from js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,34 +4,14 @@ $(document).ready(function(){
         addInfo()
         function addMaps(){
             var mapLocation = $("#maps")
-            var maps = $("<div id='map' class='column'>")
-                .css({
-                    "height":"500px",
-                    "margin":"30px",
-                    "margin-bottom":"0px",
-                    "padding":"10px",
-                    "border":"5px solid blue"
-                    })
+            var maps = $("<div id='map' class='column'>");
             mapLocation.append(maps)
         }
         function addInfo(){
             var locationInfo = $("#locationInfo")
-            var topSites = $("<div id='sites' class='column is-7'>")
-                .css({
-                    "height":"500px",
-                    "margin":"30px",
-                    "margin-right":"0px",
-                    "padding":"5px",
-                    "border":"5px solid red"
-                    })
-            var weatherDashboard = $("<div id='weather' class='column is-4'>")
-                .css({
-                    "height":"500px",
-                    "margin":"30px",
-                    "margin-left":"10px",
-                    "padding":"5px",
-                    "border":"5px solid green"
-                    })
+            var topSites = $("<div id='sites' class='column is-7'>");
+            var weatherDashboard = $("<div id='weather' class='column is-4'>");
+            
             locationInfo.append(topSites)
             locationInfo.append(weatherDashboard)
         }
