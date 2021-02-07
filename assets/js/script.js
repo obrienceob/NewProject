45604d8b17a2d322c56e3de63dbb56154448e7e9
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-02-06 22:08:05 -0800

    added remove to stop it from populating multiple areas, added local storage commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,40 @@
 $(document).ready(function(){
+    //weather API
     var weatherAPI = "424b27cb93fafd7914e312602e3d2a39";
-    $("#weather").empty();
-  
+    //city array for local storage
+    var cityI = [];
+
+    //gets local storage
+    if(localStorage.getItem("city") !== null) {
+    cityI = JSON.parse(localStorage.getItem("city"));
+    }
+    
+
+    //click function
     $("#search-btn").on('click', function(){
+    
+        //var for search value
         var searchCity = $("#search-places").val();
-        $("#weather").empty();
-        $("#sites").empty();
-        $("#map").empty();
+
+        //local storage save
+        cityI.push($("#search-places").val());
+        window.localStorage.setItem("city", JSON.stringify(cityI));
+        
+
+        //removes the prevous info when search again
+        $(".weathT").remove();
+        $(".maps").remove();
+        $(".site").remove()
+        
 
         //clears the users window afterwards
          if($("#search-places").val()) {
             $("#search-places").val("");
         }
         
-
-        addInfo()
-        
-        function addInfo(){
-            getWeather(searchCity);
-            getForecast(searchCity);
-        }
-
-         
+        //function call for weather
+        getWeather(searchCity);
+        getForecast(searchCity);
         
     })
 
@@ -32,21 +45,21 @@ $(document).ready(function(){
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + weatherAPI + "&units=imperial",
             dataType: "json",
             success: function(data) {
-                //empties weather area
-              
-                $("#weather").empty();
-
+                
+                //creates map area and appends to map
                 var mapLocation = $("#maps")
-                var maps = $("<div id='map' class='column'>");
+                var maps = $("<div id='map' class='column maps'>");
                 mapLocation.append(maps);
 
+                //creates the sites and weather
                 var locationInfo = $("#locationInfo")
-                var topSites = $("<div id='sites' class='column is-7'>");
-                var weatherDashboard = $("<div id='weather' class='column is-4'>");
-
+                var topSites = $("<div id='sites' class='column is-7 site'>");
+                var weatherDashboard = $("<div id='weather' class='column is-4 weathT'>");
+                //appends to them 
                 locationInfo.append(topSites);
                 locationInfo.append(weatherDashboard);
 
+                //weather title, city searched
                 var title = data.name;
              
                 //creates the card, adds the info from the data from the api to the card
@@ -60,24 +73,25 @@ $(document).ready(function(){
                 weatherTitle.append(image);
                 card.append(weatherTitle, temperature);
                 weatherCard.append(card);
-                weatherDashboard.append(weatherCard);
-
+                $("#weather").append(weatherCard);
             },
 
+            //catches error and pops up an error window
             error: function(textStatus, errorThrown) {
-                console.log(textStatus);
-                console.log(errorThrown);
+                //creates the content
                 var popUpOL = $('<div class="popup-overlay">');
                 var popup = $('<div class="popup-content">');
                 var popUInfo = $('<h2 class="errtxt">').text("Error");
                 var popP = $('<p class="errp">').text("The City searched was not found, Try Again.");
                 var closepbtn = $('<button class="close">').text("close");
-
-        
+    
+                //appends it to the html
                 $(".error").append(popUpOL.append(popup.append(popUInfo, popP, closepbtn)));
 
+                //creates the popup content class for it to be active and popup
                 $(".popup-overlay, .popup-content").addClass("active");
                 
+                //when close is clicked then removes class 
                 $(".close, .popup-overlay").on("click", function() {
                     $(".popup-overlay, .popup-content").removeClass("active");
                   });
@@ -136,12 +150,13 @@ $(document).ready(function(){
 
                     }
                 }
-
+                //function for map call
                 initMap(latitude, longitude);    
             }  
-
         }
     )};
+
+    //map function for map uses google map api
     let map;
     function initMap(latitude, longitude) {
         const myLatLng = { lat: latitude, lng: longitude };
