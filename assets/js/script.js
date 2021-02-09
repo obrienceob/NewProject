0caaa47e1175f2443714669a5cabbb1dacb3a82b
Sandu Corobcenco <corobcenco.sandu@gmail.com>
2021-02-09 15:20:06 -0800

    changed function intiMap(), added Markers on MAP. Don't merge to main, test it '

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,39 @@
 $(document).ready(function(){
+    //weather API
     var weatherAPI = "424b27cb93fafd7914e312602e3d2a39";
-    $("#weather").empty();
-  
+    var googleAPI = "AIzaSyA9KoDHkh0ImS4kCiFNvslAws_sa4MVNXE";
+
+    //city array for local storage
+    var cityI = [];
+
+    //gets local storage, saves to array
+    if(localStorage.getItem("city") !== null) {
+    cityI = JSON.parse(localStorage.getItem("city"));
+    }
+    
+
+    //click function
     $("#search-btn").on('click', function(){
+        
+        //var for search value
         var searchCity = $("#search-places").val();
-        $("#weather").empty();
+
+
+        //removes the prevous info when search again
+        $(".weathT").remove();
+        $(".maps").remove();
+        $("#sites").addClass("is-hidden");
+        
+        
 
         //clears the users window afterwards
          if($("#search-places").val()) {
             $("#search-places").val("");
         }
         
-        addMaps()
-        addInfo()
-        function addMaps(){
-            var mapLocation = $("#maps")
-            var maps = $("<div id='map' class='column'>");
-            mapLocation.append(maps)
-        }
-        function addInfo(){
-            
-            var locationInfo = $("#locationInfo")
-            var topSites = $("<div id='sites' class='column is-7'>");
-            var weatherDashboard = $("<div id='weather' class='column is-4'>");
-
-            getWeather(searchCity);
-            getForecast(searchCity);
-            
-
-        
-            locationInfo.append(topSites)
-            locationInfo.append(weatherDashboard)
-        }
-
-         
+        //function call for weather
+        getWeather(searchCity);
+        getForecast(searchCity);
         
     })
 
@@ -44,9 +44,21 @@ $(document).ready(function(){
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + weatherAPI + "&units=imperial",
             dataType: "json",
             success: function(data) {
-                //empties weather area
-                $("#weather").empty();
+                //removes attr from site
+                $("#sites").removeAttr("class").attr("class", "column is-7")
+                
+                //creates map area and appends to map
+                var mapLocation = $("#maps")
+                var maps = $("<div id='map' class='column maps'>");
+                mapLocation.append(maps);
 
+                //creates the sites and weather
+                var locationInfo = $("#locationInfo")
+                var weatherDashboard = $("<div id='weather' class='column is-4 weathT'>");
+                //appends to them 
+                locationInfo.append(weatherDashboard);
+
+                //weather title, city searched
                 var title = data.name;
              
                 //creates the card, adds the info from the data from the api to the card
@@ -61,8 +73,30 @@ $(document).ready(function(){
                 card.append(weatherTitle, temperature);
                 weatherCard.append(card);
                 $("#weather").append(weatherCard);
+            },
 
+            //catches error and pops up an error window
+            error: function(textStatus, errorThrown) {
+                //creates the content
+                var popUpOL = $('<div class="popup-overlay">');
+                var popup = $('<div class="popup-content">');
+                var popUInfo = $('<h2 class="errtxt">').text("Error");
+                var popP = $('<p class="errp">').text("The City searched was not found, Try Again.");
+                var closepbtn = $('<button class="close">').text("close");
+
+    
+                //appends it to the html
+                $(".error").append(popUpOL.append(popup.append(popUInfo, popP, list, closepbtn)));
+
+                //creates the popup content class for it to be active and popup
+                $(".popup-overlay, .popup-content").addClass("active");
+                
+                //when close is clicked then removes class 
+                $(".close, .popup-overlay").on("click", function() {
+                    $(".popup-overlay, .popup-content").removeClass("active");
+                  });
             }
+
         }
     )}
 
@@ -116,18 +150,97 @@ $(document).ready(function(){
 
                     }
                 }
-
-                initMap(latitude, longitude);    
+                //function for map call
+                initMap(latitude, longitude); 
+                findPlaces(searchCity);
+                
             }  
-
         }
     )};
+
+    //map function for map uses google map api
     let map;
-    function initMap(latitude, longitude) {
-        const myLatLng = { lat: latitude, lng: longitude };
-        const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
-        center: myLatLng,
+    function initMap(latitude,longitude) {
+        var myLatlng = new google.maps.LatLng(latitude, longitude);
+        var myOptions = {
+            zoom: 11,
+            center: myLatlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        map = new google.maps.Map(document.getElementById("map"), myOptions);
+        
+    }
+    
+    // Function for adding a marker to the page.
+    function addMarker(location) {
+        marker = new google.maps.Marker({
+            position: location,
+            map: map,
+
         });
-    }  
+        
+    }
+
+
+    function addInfoWindow(marker, message) {
+
+        var infoWindow = new google.maps.InfoWindow({
+            content: message
+        });
+
+        
+            infoWindow.open(map, marker);
+        
+    }
+
+    // Testing the addMarker function
+    function TestMarker(latVar, longVar) {
+           locationVar = new google.maps.LatLng(latVar, longVar);
+           addMarker(locationVar);
+    }
+    
+
+      
+
+
+    function findPlaces(city){
+        var categories = $(".categoryImage")
+        categories.on("click", function(){
+        $("#row-1").removeAttr("class").attr("class", "columns is-1")
+        $("#row-2").removeAttr("class").attr("class", "columns is-1")
+        var selection = $(this).attr("id")
+        
+        var proxyurl = "https://cors-anywhere.herokuapp.com/";
+        var url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${selection}+in+${city}&key=${googleAPI}`; // site that doesn’t send Access-Control-*
+        
+        fetch(proxyurl + url) 
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            var results = data.results
+            console.log(data)
+            for( var i = 1; i < 7; ++i ){
+               var photoRef = results[i].photos[0].photo_reference
+               var picUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRef}&key=${googleAPI}`
+               $("#placeImage-"+i).attr("src", picUrl)
+               $("#name-"+i).text(data.results[i].name)
+               $("#rating-"+i).text(data.results[i].rating)
+
+                var latVar = data.results[i].geometry.location.lat;
+                var longVar = data.results[i].geometry.location.lng;
+                var content = data.results[i].name;
+
+               TestMarker(latVar, longVar);
+               addInfoWindow(marker, content);
+            }
+        })
+        .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+    })
+
+    //local storage save
+    cityI.push(city);
+    window.localStorage.setItem("city", JSON.stringify(cityI));
+    
+    } // End function
 });
